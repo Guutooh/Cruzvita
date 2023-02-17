5bda1ceff8c7a0fae581bc6d85f897040c188058
Guutooh <gustavo.oliveiradossantos@gmail.com>
2023-02-16 22:19:35 -0300

    efetuado alterações do Exercício implementando POO

---
+++ atividade-mvn/src/test/java/br/com/cruzvita/AtividadeMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cruzvita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
